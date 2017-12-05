0e58e9768025f9e964eb96cdd3eb8143a402cd77
vrsh29 <vrsh08@gmail.com>
2017-12-05 09:51:12 +0530

    COMMIT

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.css']
 })
-export class SearchPageComponent implements OnInit {
 
-  onSearch()
-  {
+export class SearchPageComponent {
+  num = [1, 2, 3 , 4, 5, 6, 7, 8];
+  heroForm: FormGroup;
 
+  constructor(private fb: FormBuilder){
+    this.createForm();
   }
 
-  constructor() { }
-  ngOnInit() {
+  createForm() {
+    this.heroForm = this.fb.group({
+      Search_String: [''],
+      Maximum_Result: '',
+    });
   }
-
 }
