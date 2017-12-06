dc2312f85eafbaf25658f54af4103691c876554e
vrsh29 <vrsh08@gmail.com>
2017-12-06 06:41:02 +0530

    Add files via upload

--- src/app/results-page/results-page.component.ts
+++ src/app/results-page/results-page.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { RouterModule, Routes, Router} from '@angular/router';
 import { DataService } from '../data.service';
 import { FormControl, FormGroup , Validators} from '@angular/forms';
+import 'rxjs/add/operator/map';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-results-page',
@@ -11,17 +13,18 @@ import { FormControl, FormGroup , Validators} from '@angular/forms';
 })
 export class ResultsPageComponent implements OnInit {
   hotel:any;
-
-  searchValue: string;
+  searchValue: any;
   result: any;
-  constructor(private route: ActivatedRoute, private dataService: DataService) { }
  
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private dataService: DataService) { }
+
   ngOnInit() 
   {
-   this.route.params.subscribe(params => {
+   this.activatedRoute.params.subscribe(params =>{
 
-   //this.dataService.getHotelDetails(params['location'],3).subscribe(res => this.hotel=res );
-   console.log("obj"+this.hotel);
-  });
-  }
-  }
+     this.dataService.getHotelDetails(params['location'],5).subscribe(res => this.hotel = res);
+   //console.log("obj"+this.hotel);
+     }
+    )
+}
+}
--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl, FormsModule,  } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl, FormsModule, AbstractControl } from '@angular/forms';
 import {ReactiveFormsModule} from '@angular/forms';
 import { Router, RouterModule, Routes } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-search-page',
@@ -10,20 +11,31 @@ import { Router, RouterModule, Routes } from '@angular/router';
 })
 
 export class SearchPageComponent {
-num = [1, 2, 3 , 4, 5, 6, 7, 8];
 
-  // numForm = new FormGroup({
-  //   MaxResults: new FormControl
-  // });
+  number: number[];
+  formGroup: FormGroup;
+  search: AbstractControl;
+  numb: AbstractControl;
+  hotelForm:any;
+  users;
 
-  // hotelForm= new FormGroup({
-  //   Search_String: new FormControl
-  // });
 
-  constructor(private fb: FormBuilder, private router: Router) {
-  }
+  constructor(private fb: FormBuilder, private router: Router, private _dataService: DataService) {
+    this.number = [1, 2, 3, 4, 5, 6, 7, 8];
+    
+    this.formGroup = fb.group({
+      "numb": ['', Validators.required],
+      "search": ['', Validators.required]
+    })
+    this.numb = this.formGroup.controls['numb'];
+    this.search = this.formGroup.controls['search'];
+
+    // this._dataService.getHotelDetails(location, maxResult)()
+    //   .subscribe(res => this.users = res);
+      }
 
   doSearch(value){
-  this.router.navigate(['result'],value.Search_String);
+    this.router.navigate(['results',value.Search_String,5]);
+    }
 }
-}
+
